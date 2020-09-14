c4c3045c662c7e5273f1a9fff985f13b575ff24d
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-14 19:23:55 -0400

    imported our form component to Login

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Button from './Button'
-
 import Input from './Input'
 import userInputs from '../utils/userInputs'
+import Form from './Form'
 
 export default function Login() {
     const regLink = '/register';
@@ -12,6 +12,7 @@ export default function Login() {
                 Login
             </h1>
             <br/>
+            <Form inputs={userInputs} title="Login"/>
             <Input placeholder="Enter Name" name="name" type="text" />
             <br/>
             <Button onClick={() => {window.location = regLink}} text="Register"/>
