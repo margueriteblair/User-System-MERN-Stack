fea654eae72b339501a59609a823c2adebe032f8
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-14 18:38:25 -0400

    made default styling objects for input Component

---
+++ client/src/Components/Input.js
@@ -0,0 +1,28 @@
+import React from 'react'
+        //placeholder
+        //type
+        //onChange function
+        //name for the input
+
+export default function Input(props) {
+
+    return (
+        <input 
+        id={props.id}
+        name={props.name}
+        onChange={props.onChange}
+        type={props.type}
+        placeholder={props.placeholder}
+        style={{...defautStyle, ...props.style}}
+        />
+    )
+}
+
+const defautStyle = {
+    color: 'black',
+    background: 'white',
+    border: 'none',
+    padding: 10,
+    borderRadius: 5
+}
+
