d0ef2b259bfba8509c9959b16febccd57278a5fc
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-14 19:27:03 -0400

    fixed error with the user inputs page

--- client/src/utils/userInputs.js
+++ client/src/utils/userInputs.js
@@ -1,7 +1,6 @@
-module.exports = {
-    [
-        {name: 'username', ph: 'Enter Username', type: 'text'},
-        {name: 'email', ph: 'Enter Email', type: 'text'},
-        {name: 'password', ph: 'Enter Password', type: 'password'}
-      ]
-}
+module.exports = [
+    {name: 'username', ph: 'Enter Username', type: 'text'},
+    {name: 'email', ph: 'Enter Email', type: 'text'},
+    {name: 'password', ph: 'Enter Password', type: 'password'}
+
+]
