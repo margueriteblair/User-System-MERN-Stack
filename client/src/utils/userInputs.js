5781d9cb94fdfd1ec835ec780f9838652bc7d359
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-14 21:20:19 -0400

    modified the user inputs

--- client/src/utils/userInputs.js
+++ client/src/utils/userInputs.js
@@ -1,6 +1,13 @@
-module.exports = [
-    {name: 'username', ph: 'Enter Username', type: 'text'},
-    {name: 'email', ph: 'Enter Email', type: 'text'},
+module.exports = {
+loginInputs: [
+    {name: 'credential', ph: 'Enter Username or Email', type: 'text'},
     {name: 'password', ph: 'Enter Password', type: 'password'}
 
+],
+reqInputs: [
+    {name: 'username', ph: 'Enter Username', type: 'text'},
+    {name: 'email', ph: 'Enter Email', type: 'text'},
+    {name: 'password', ph: 'Enter Password', type: 'password'},
+    {name: 'password2', ph: 'Enter Password Again', type: 'password'}
 ]
+}
