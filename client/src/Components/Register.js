0ad3eb7ed4747e3b51b297df2c862e06f1b43e53
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-14 20:11:56 -0400

    altered app component

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Button from './Button';
+import userInputs from '../utils/userInputs';
+import Form from './Form'
+import { regReq } from '../utils/userRequests'
 
 export default function Register() {
     const loginLink = '/login';
@@ -9,6 +12,9 @@ export default function Register() {
                 Welcome To Our Service
             </h1>
             <br/>
+            <Form title="Register" inputs={[...userInputs, {name: 'password2', placeholder: "Enter Password Again", type: "password"}]}
+                submitFunc={regReq}
+            />
             <Button onClick={() =>{window.location = loginLink}} text="Login Button"/>
             <br/>
         </div>
---
+++ client/src/utils/userRequests.js
@@ -0,0 +1,11 @@
+const { default: Form } = require("../../../../../../../../../../../../Form")
+
+module.exports = {
+    loginReq: (form) => {
+        console.log(form)
+    },
+
+    regReq: (form) => {
+        console.log(form)
+    }
+}
