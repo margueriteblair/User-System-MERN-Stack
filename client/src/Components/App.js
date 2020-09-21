b4938b7b8a8b49bfa74c7f7688c6bb8a075b1ffd
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-21 17:11:07 -0400

    Light mode and dark mode buttons with better routing

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
+// import {BrowserRouter} from 'react-router-dom'
 import AppRouter from './AppRouter'
 import Button from './Button'
 import Styles from '../utils/styles'
 
-function App() {
-  return (
-    < BrowserRouter>
-    <Button text='Dark Mode' onClick={() => {Styles.currentStyle = Styles.dark; this.setState({style: Styles.dark})}}/>
-    <Button text='Light Mode' onClick={() => {
-      Styles.currentStyle = Styles.light;
-      this.setState({styles: Styles.light})
-    }}/>
-        <AppRouter />
-    </BrowserRouter>
-  );
-}
+export default class App extends React.Component { //TODO: Create AppRouter
 
-export default App;
+  constructor() {
+    super();
+    this.state = {
+      style: Styles.currentStyle
+    };
+  }
+
+  render() {
+    return (
+      <div className={"App " + this.state.style}>
+        <Button 
+          text='Dark Mode'
+          onClick={() => {
+            Styles.currentStyle = Styles.dark; 
+            this.setState({style: Styles.dark});
+          }}
+        />
+        <Button 
+          text='Light Mode'
+          onClick={() => {
+            Styles.currentStyle = Styles.light; 
+            this.setState({style: Styles.light});
+          }}
+        />
+        <hr/>
+        <AppRouter/>
+        <hr/>
+      </div>
+    );
+  }
+}
