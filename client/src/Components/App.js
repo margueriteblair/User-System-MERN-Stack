39656dc43c040838ece8596ab77f5b47b16e6409
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-21 17:00:01 -0400

    created two new button instances for light and dark mode

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom'
 import AppRouter from './AppRouter'
+import Button from './Button'
+import Styles from '../utils/styles'
 
 function App() {
   return (
     < BrowserRouter>
+    <Button text='Dark Mode' onClick={() => {Styles.currentStyle = Styles.dark; this.setState({style: Styles.dark})}}/>
+    <Button text='Light Mode' onClick={() => {
+      Styles.currentStyle = Styles.light;
+      this.setState({styles: Styles.light})
+    }}/>
         <AppRouter />
     </BrowserRouter>
   );
---
+++ client/src/utils/styles.js
@@ -0,0 +1,7 @@
+export default class Styler {
+
+
+    static currentStyle = 'light';
+    static dark = 'dark';
+    static light = 'light'
+}
