33b6be6a57f1da46806fe76f05e7a04a24249994
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-21 21:45:10 -0400

    use effect react method

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,39 +1,18 @@
 import React, { useState, useEffect, useContext } from 'react';
-// import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import AppRouter from './AppRouter'
 import Button from './Button'
 import Styles from '../utils/styles'
 
-export default class App extends React.Component { //TODO: Create AppRouter
 
-  constructor() {
-    super();
-    this.state = {
-      style: Styles.currentStyle
-    };
-  }
+export default function App() {
+  const [theme, setTheme ] = useState(true)
+  useEffect( () => {
 
-  render() {
-    return (
-      <div className={"App " + this.state.style}>
-        <Button 
-          text='Dark Mode'
-          onClick={() => {
-            Styles.currentStyle = Styles.dark; 
-            this.setState({style: Styles.dark});
-          }}
-        />
-        <Button 
-          text='Light Mode'
-          onClick={() => {
-            Styles.currentStyle = Styles.light; 
-            this.setState({style: Styles.light});
-          }}
-        />
-        <hr/>
-        <AppRouter/>
-        <hr/>
-      </div>
-    );
-  }
+  }, [])
+   return (
+    <BrowserRouter>
+    <AppRouter />
+    </BrowserRouter>
+  )
 }
