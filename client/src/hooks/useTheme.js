33f0113362afd15ea3f9b1ac43282a5ad332e2cf
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-21 22:08:20 -0400

    local storage util

--- client/src/hooks/useTheme.js
+++ client/src/hooks/useTheme.js
@@ -1,6 +1,19 @@
 import React, {useState, useEffect} from 'react'
+import {get, set} from '../utils/localStorage'
+
 
 export default useTheme(initialVal) {
-    
+    const [theme, setTheme] = useState(() => {
+        return initialVal
+    })
     return [theme, setTheme]
+}
+
+function get (key) {
+
+}
+
+
+function set (key, value) {
+
 }
