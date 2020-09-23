dcb62b36268c5a0201c629fb2253b7baf64bed33
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-22 20:46:32 -0400

    setter and getter imported into theme context

--- client/src/hooks/ThemeContext.js
+++ client/src/hooks/ThemeContext.js
@@ -1,1 +1,25 @@
 import React, { useState, useEffect, useContext, createContext } from 'react'
+
+
+const {get, set} = require('../utils/localStorage')
+
+const ThemeContext = createContext()
+
+const ThemeUpdateContext = createContext()
+
+export function ThemeProvider({childen}) {
+    const [theme, setTheme] = useState( () => {
+        return get('theme', true)
+    })
+
+    useEffect(() => {
+        set('theme', JSON.stringify(theme))
+        console.log('The theme changed: ', theme)
+    }, [theme]) //the dependency list, what needs to change for the function to run
+    //only runs the function when theme changes
+
+    return (
+        <>
+        </>
+    )
+}
