92793f9f976f061d3ce6b4f444bcf9e6747693a5
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-22 20:54:03 -0400

    use theme provier

--- client/src/hooks/ThemeContext.js
+++ client/src/hooks/ThemeContext.js
@@ -7,6 +7,14 @@ const ThemeContext = createContext()
 
 const ThemeUpdateContext = createContext()
 
+export function useTheme() {
+    return useContext(ThemeContext)
+}
+
+export function useThemeUpdate() {
+    return useContext(ThemeUpdateContext)
+}
+
 export function ThemeProvider({childen}) {
     const [theme, setTheme] = useState( () => {
         return get('theme', true)
@@ -17,9 +25,14 @@ export function ThemeProvider({childen}) {
         console.log('The theme changed: ', theme)
     }, [theme]) //the dependency list, what needs to change for the function to run
     //only runs the function when theme changes
-
+    const themeToggle = () => {
+        setTheme(prev => !prev)
+    }
     return (
-        <>
-        </>
+        <ThemeContext.Provider value={theme}>
+            <ThemeUpdateContext value={themeToggle}>
+                {{children}}
+            </ThemeUpdateContext>
+        </ThemeContext.Provider>
     )
 }
