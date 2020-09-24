762a59ecac436c2449cdf491878ea0ff62caeecb
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-23 21:51:23 -0400

    home js component fixed

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,28 +1,35 @@
-import React from 'react'
-import Button from './Button'
-import {useTheme} from '../hooks/useTheme'
+import React from 'react';
+import Button from './Button';
+// import useTheme from '../hooks/useTheme'
+import {useTheme} from '../hooks/ThemeContext'
 
 export default function Home() {
+
     const loginLink = '/login';
     const regLink = '/register';
+//   const [theme] = useTheme()
+
     return (
         <div>
             <h1>
                 Welcome To Our Service
             </h1>
             <h2>
-                {useTheme() ? 'Light Mode' : 'Dark Mode'}
+                {useTheme() ? "You're in Light" : "You're in Dark"}
             </h2>
-
             <br/>
 
             <Button 
             onClick= {() => {window.location = loginLink}}
-            text='Login Button'
+            text='Login button'
+            style={{color: 'white', backgroundColor: 'black'}}
             />
-            <br/>
-            <Button onClick={() => {window.location = regLink}} text="Register Button"/>
 
+            <Button 
+            onClick= {() => {window.location = regLink}}
+            text='Register button'
+            style={{color: 'white', backgroundColor: 'black'}}
+            />
         </div>
     )
 }
