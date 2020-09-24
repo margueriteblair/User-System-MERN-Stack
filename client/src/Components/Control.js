c088ec0b71b415bc4d730daf51d3f922ebdca32a
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-23 21:34:58 -0400

    control component updated

--- client/src/Components/Control.js
+++ client/src/Components/Control.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import {useTheme, useThemeUpdate} from '../hooks/ThemeContext'
 
-//any array can be taken, it's just an initial value
-
 export default function Control() {
 
-    const theme = useTheme()
+    const theme = useTheme();
+    const updateTheme = useThemeUpdate();
 
-    const updateTheme = useThemeUpdate()
-
-    console.log(theme)
-
-    // setTheme(() => {})
-
+    //setTheme()
+    
     return (
         <div>
-            <h1>{theme ? 'Light Mode' : "Dark Mode"}</h1>
-            <button
+        <h1>
+            {theme ? 'Light Mode' : 'Dark Mode'}
+        </h1>
+
+        <button
             onClick={updateTheme}
-            >Toggle Theme</button>
+        >
+            Toggle Theme 
+        </button>
         </div>
     )
 }
